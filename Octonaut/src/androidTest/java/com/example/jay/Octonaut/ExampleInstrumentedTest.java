1af99deddc0c873b7a29f3ce46f9aa31dfc4f84c
justinlautner <justinjlautner@gmail.com>
2020-11-14 15:20:00 -0600

    Upgrade syntax to match AndroidX specifications

--- Octonaut/src/androidTest/java/com/example/jay/Octonaut/ExampleInstrumentedTest.java
+++ Octonaut/src/androidTest/java/com/example/jay/Octonaut/ExampleInstrumentedTest.java
@@ -1,5 +1,6 @@
 package com.example.jay.Octonaut;
 
+import android.app.Instrumentation;
 import android.content.Context;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -19,8 +20,8 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        Instrumentation appContext = InstrumentationRegistry.getInstrumentation();
 
-        assertEquals("com.example.jay.gameproject", appContext.getPackageName());
+        assertEquals("com.example.jay.gameproject", appContext.getProcessName());
     }
 }
